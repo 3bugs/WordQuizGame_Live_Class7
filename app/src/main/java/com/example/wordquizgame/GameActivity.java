c303820c2b9addd60f7870a071ee66edeb4f5899
promlert <promlert@gmail.com>
2015-08-09 12:50:29 +0700

    3rd commit

--- app/src/main/java/com/example/wordquizgame/GameActivity.java
+++ app/src/main/java/com/example/wordquizgame/GameActivity.java
@@ -1,10 +1,11 @@
 package com.example.wordquizgame;
 
-import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,18 +14,15 @@ import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.Headers;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class GameActivity extends ActionBarActivity {
 
     private final String TAG = "GameActivity";
+    private final int TOTAL_QUESTIONS = 5;
 
     private int difficulty;
 
@@ -41,6 +39,9 @@ public class GameActivity extends ActionBarActivity {
     private ImageView questionImageView;
     private TableLayout buttonTableLayout;
     private TextView answerTextView;
+
+    private Random random;
+    private Handler handler;
 
 
     @Override
@@ -77,9 +78,119 @@ public class GameActivity extends ActionBarActivity {
         buttonTableLayout = (TableLayout) findViewById(R.id.buttonTableLayout);
         answerTextView = (TextView) findViewById(R.id.answerTextView);
 
-        
+        fileNameList = new ArrayList<String>();
+        quizWordsList = new ArrayList<String>();
+        choiceWords = new ArrayList<String>();
+
+        random = new Random();
+        handler = new Handler();
+
+        getImageFileName();
     }
 
+    private void getImageFileName() {
+        String[] categories = { "animals", "body", "colors", "numbers", "objects" };
+
+        AssetManager assets = getAssets();
+
+        for (String category : categories) {
+            try {
+                String[] fileNames = assets.list(category);
+
+                for (String fileName : fileNames) {
+                    fileNameList.add(fileName.replace(".png", ""));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Log.i(TAG, "***** รายชื่อไฟล์ทั้งหมด *****");
+        for (String fileName : fileNameList) {
+            Log.i(TAG, fileName);
+        }
+
+        startQuiz();
+    }
+
+    private void startQuiz() {
+        totalGuesses = 0;
+        score = 0;
+        quizWordsList.clear();
+
+        while (quizWordsList.size() < TOTAL_QUESTIONS) {
+            int randomIndex = random.nextInt(fileNameList.size());
+            String fileName = fileNameList.get(randomIndex);
+
+            if (quizWordsList.contains(fileName) == false) {
+                quizWordsList.add(fileName);
+            }
+        }
+
+        Log.i(TAG, "***** ชื่อไฟล์สำหรับตั้งโจทย์ที่สุ่มได้ *****");
+        for (String fileName : quizWordsList) {
+            Log.i(TAG, fileName);
+        }
+
+        loadNextQuestion();
+    }
+
+    private void loadNextQuestion() {
+        answerTextView.setText(null);
+        answerFileName = quizWordsList.remove(0);
+
+        String msg = String.format("คำถามข้อที่ %d จากทั้งหมด %d ข้อ", score + 1, TOTAL_QUESTIONS);
+        questionNumberTextView.setText(msg);
+
+        loadQuestionImage();
+        prepareChoiceWords();
+    }
+
+    private void loadQuestionImage() {
+        String category = answerFileName.substring(0, answerFileName.indexOf('-'));
+        String filePath = category + "/" + answerFileName + ".png";
+
+        AssetManager assets = getAssets();
+        InputStream stream;
+
+        try {
+            stream = assets.open(filePath);
+
+            Drawable image = Drawable.createFromStream(stream, filePath);
+            questionImageView.setImageDrawable(image);
+
+        } catch (IOException e) {
+            Log.e(TAG, "Error loading file: " + filePath);
+            e.printStackTrace();
+        }
+    }
+
+    private void prepareChoiceWords() {
+        choiceWords.clear();
+
+        while (choiceWords.size() < numChoices) {
+            int randomIndex = random.nextInt(fileNameList.size());
+            String randomWord = getWord(fileNameList.get(randomIndex));
+
+            if (choiceWords.contains(randomWord) == false
+                    && randomWord.equals(getWord(answerFileName)) == false) {
+                choiceWords.add(randomWord);
+            }
+        }
+
+        int randomIndex = random.nextInt(numChoices);
+        choiceWords.set(randomIndex, getWord(answerFileName));
+
+        Log.i(TAG, "***** คำศัพท์ตัวเลือกที่สุ่มได้ *****");
+        for (String word : choiceWords) {
+            Log.i(TAG, word);
+        }
+    }
+
+    private String getWord(String fileName) {
+        String word = fileName.substring(fileName.indexOf('-') + 1);
+        return word;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
